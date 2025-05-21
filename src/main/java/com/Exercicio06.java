d35f29c24a45ff5aaa36950159780286213bbc14
Ezequias Prado <ezequias.moreira.do.prado@gmail.com>
2025-05-20 21:42:53 -0300

    Criado o exercício 6

---
+++ src/main/java/com/Exercicio06.java
@@ -0,0 +1,20 @@
+package main.java.com;
+
+import java.util.Scanner;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o nome do produto:");
+        String nome = scanner.nextLine();
+        System.out.println("Digite o preço de compra");
+        double precoCompra = scanner.nextDouble();
+        System.out.println("Digite o preço de venda");
+        double precoVenda = scanner.nextDouble();
+
+        double lucro = precoVenda - precoCompra;
+
+        System.out.println("O lucro do produto: " + nome + " é : " + lucro);
+
+    }
+}
